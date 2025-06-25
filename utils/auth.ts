3e8a59fff0b1cdc58fc26bf2f4125bcb3032af2d
Melanie-fx <gideongodwill183@gmail.com>
2025-06-25 15:34:45 +0100

    Update auth.ts

--- utils/auth.ts
+++ utils/auth.ts
@@ -7,13 +7,13 @@ export async function signUp(email: string, password: string) {
   return error
 }
 
-// Log in
+// Log in existing user
 export async function signIn(email: string, password: string) {
   const { error } = await supabase.auth.signInWithPassword({ email, password })
   return error
 }
 
-// Log out
+// Log out current user
 export async function signOut() {
   await supabase.auth.signOut()
 }
