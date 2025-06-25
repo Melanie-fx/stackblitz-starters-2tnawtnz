accf608bcf4a389320f4edc1fa57402cc8da54a5
Melanie-fx <gideongodwill183@gmail.com>
2025-06-25 11:48:46 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-}
+  // You can leave this empty or add other config options here
+  reactStrictMode: true,
+  swcMinify: true,
+  // If you're using server components or special features, add them here
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
