328f888826d9325530cd99195c2c8c51a3622a2c
Melanie-fx <gideongodwill183@gmail.com>
2025-06-25 12:56:00 +0100

    Update route.ts

--- app/auth/logout/route.ts
+++ app/auth/logout/route.ts
@@ -1,5 +1,3 @@
-// app/auth/logout/route.ts
-
 'use server'
 
 import { cookies } from 'next/headers'
@@ -13,7 +11,7 @@ export async function GET() {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: () => cookieStore
+      cookies: () => cookieStore,
     }
   )
 
