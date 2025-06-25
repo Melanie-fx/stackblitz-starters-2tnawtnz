a0c15e3176f60928714c2e78bea89bc7c84966dd
Melanie-fx <gideongodwill183@gmail.com>
2025-06-25 16:35:17 +0100

    Update server.ts

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,15 +1,18 @@
 // utils/supabase/server.ts
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
 
 export const createSupabaseServerClient = () => {
-  const cookieStore = cookies()
-
+  const cookieStore = cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: () => cookieStore
+      cookies: {
+        get: (name) => cookieStore.get(name)?.value,
+        set: (name, value, options) => cookieStore.set(name, value, options),
+        remove: (name, options) => cookieStore.delete({ name, ...options }),
+      },
     }
-  )
-}
+  );
+};
