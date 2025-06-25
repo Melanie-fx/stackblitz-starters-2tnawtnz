4ed6f1a98a74376de20659106c49d8fd4d73fbf6
Melanie-fx <gideongodwill183@gmail.com>
2025-06-25 11:57:15 +0100

    Update supabase.ts

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,10 @@
+'use client';
+
 import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+export const createClient = () => {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+};
