7163f49514c4fa313d1329930a6f04da855d8870
Melanie-fx <gideongodwill183@gmail.com>
2025-06-25 12:51:37 +0100

    Update supabase.ts

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,10 +3,20 @@
 'use client'
 
 import { createBrowserClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+import { createServerClient } from '@supabase/ssr'
 
 export const createClient = () => {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
+
+export const createServer = () => {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { cookies }
   )
 }
