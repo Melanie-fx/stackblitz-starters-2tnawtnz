89236055a1d69a1e0e1d5db70f9a7a9adc9fb44d
Melanie-fx <gideongodwill183@gmail.com>
2025-06-25 15:52:17 +0100

    Update page.tsx

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { supabase } from '@/utils/supabase'
+import { supabase } from '@/utils/supabase/client';
 import { useRouter } from 'next/navigation'
 
 type Video = {
