fe772256f646263a7c165fed448af56118e33a97
Melanie-fx <gideongodwill183@gmail.com>
2025-06-25 17:31:52 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,14 @@
 // middleware.ts
-import { NextRequest, NextResponse } from 'next/server'
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextRequest, NextResponse } from 'next/server';
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import type { Database } from './types/supabase'; // optional if you defined custom types
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
+  const res = NextResponse.next();
 
-  const supabase = createMiddlewareClient({ req, res })
+  const supabase = createMiddlewareClient<Database>({ req, res });
 
-  await supabase.auth.getSession()
+  await supabase.auth.getSession(); // this sets the cookie correctly
 
-  return res
+  return res;
 }
