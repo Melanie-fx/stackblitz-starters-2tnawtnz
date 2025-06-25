7814b5f67cad764893de9f9ec9aa546a8747f6c6
Melanie-fx <gideongodwill183@gmail.com>
2025-06-25 12:44:20 +0100

    Update route.ts

--- app/auth/logout/route.ts
+++ app/auth/logout/route.ts
@@ -1,9 +1,22 @@
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
+// app/auth/logout/route.ts
+
+'use server'
+
 import { cookies } from 'next/headers'
+import { createServerClient } from '@supabase/ssr'
 import { redirect } from 'next/navigation'
 
 export async function GET() {
-  const supabase = createServerActionClient({ cookies })
+  const cookieStore = cookies()
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: () => cookieStore
+    }
+  )
+
   await supabase.auth.signOut()
   redirect('/auth/login')
 }
